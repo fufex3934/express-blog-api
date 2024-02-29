4b15fe004816c76bb60e1109fc90f142e66334d5
fufex3934 <fufawakgari174@gmail.com>
2024-02-29 14:35:02 +0300

    second commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,11 @@ const signup = async (req, res, next) => {
   try {
     
     const { name, email, password, role } = req.body;
+    const isEmailExist = await User.findOne({email});
+    if(isEmailExist){
+      res.code = 400;
+      throw new Error("Email already exists");
+    }
     const newUser = new User({ name, email, password, role });
     await newUser.save();
     res
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const {authController} = require('../controllers/');
+const {signupValidator} = require('../validators/auth');
+const validate = require('../validators/validate');
 const router = express.Router();
 
-router.post('/signup',authController.signup);
+router.post('/signup',signupValidator,validate,authController.signup);
 
 module.exports = router;
